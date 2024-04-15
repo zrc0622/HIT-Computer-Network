45d4d4f82e0ecd8e1a5c019ac71e6a8bf47f68c8
stupig <1727950162@qq.com>
2024-04-15 17:39:33 +0800

    add comments

--- lab1/README.md
+++ lab1/README.md
@@ -2,6 +2,22 @@
 1. char和TCHAR：TCHAR在Unicode模式下，是wchar_t\*类型的数组；在ANSI模式下，是char\*类型的数组
 2. _tmain是VS的C++程序的入口点
 
+# 代码解读
+1. `InitSocket()`函数
+   1. 使用`WSAStartup()`初始化Socket API
+   2. 使用`socket()`创建代理套接字，返回描述符至`ProxyServer`，用于接收客户端连接
+2. `ProxyThread()`函数
+   1. 使用`recv()`接收客户端的数据
+   2. 使用自定义函数`ParseHttpHead()`解析 HTTP 头信息
+   3. 使用自定义函数`ConnectToServer()`建立与目标服务器的连接
+   4. 使用`send()`将请求转发到目标服务器
+   5. 使用`recv()`接收服务器的响应
+   6. 使用`send()`将响应返回给客户端
+3. `ParseHttpHead()`函数
+   1. 解析HTTP请求消息的HTTP头部（ASCII码格式），解析后存入HttpHeader结构体中
+   2. ![](./image/http请求消息.jpg)
+4. 
+
 # VSCode调教
 1. 点击右上角的`运行C/C++文件`
 2. 调试配置根据你的文件选择，C文件使用gcc编译，C++文件使用g++编译
--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,3 +1,4 @@
+/*引入头文件和库*/
 // #include "stdafx.h" // 需要注释
 #include <stdio.h>
 #include <Windows.h>
@@ -6,7 +7,8 @@
 #pragma comment(lib, "Ws2_32.lib")
 #define MAXSIZE 65507 // 发送数据报文的最大长度
 #define HTTP_PORT 80  // http 服务器端口
-// Http 重要头部数据
+
+/*HTTP头部结构定义*/
 struct HttpHeader
 {
     char method[4];         // POST 或者 GET，注意有些为 CONNECT，本实验暂不考虑
@@ -15,17 +17,20 @@ struct HttpHeader
     char cookie[1024 * 10]; // cookie
     HttpHeader()
     {
-        ZeroMemory(this, sizeof(HttpHeader));
+        ZeroMemory(this, sizeof(HttpHeader));   // HttpHeader()为结构体的构造函数，实例化结构体后其成员变量初始化为0
     }
 };
+
+/*函数声明*/
 BOOL InitSocket();
 void ParseHttpHead(char *buffer, HttpHeader *httpHeader);
 BOOL ConnectToServer(SOCKET *serverSocket, char *host);
 unsigned int __stdcall ProxyThread(LPVOID lpParameter);
-// 代理相关参数
-SOCKET ProxyServer;
-sockaddr_in ProxyServerAddr;
-const int ProxyPort = 10240;
+
+/*代理服务器全局变量*/
+SOCKET ProxyServer; // 代理套接字描述符：用于接收来自客户端的连接
+sockaddr_in ProxyServerAddr;    // 代理端点地址
+const int ProxyPort = 10240;    // 代理端口
 // 由于新的连接都使用新线程进行处理，对线程的频繁的创建和销毁特别浪费资源
 // 可以使用线程池技术提高服务器效率
 // const int ProxyThreadMaxNum = 20;
@@ -36,6 +41,8 @@ struct ProxyParam
     SOCKET clientSocket;
     SOCKET serverSocket;
 };
+
+/*主函数*/
 int main(int argc, char *argv[]) //  _tmain改为main，_TCHAR改为char
 {
     printf("代理服务器正在启动\n");
@@ -68,6 +75,8 @@ int main(int argc, char *argv[]) //  _tmain改为main，_TCHAR改为char
     WSACleanup();
     return 0;
 }
+
+/*函数实现*/
 //************************************
 // Method: InitSocket
 // FullName: InitSocket
@@ -83,35 +92,42 @@ BOOL InitSocket()
     // 套接字加载时错误提示
     int err;
     // 版本 2.2
-    wVersionRequested = MAKEWORD(2, 2);
+    wVersionRequested = MAKEWORD(2, 2); // 使用Winsock2.2
     // 加载 dll 文件 Scoket 库
-    err = WSAStartup(wVersionRequested, &wsaData);
+    err = WSAStartup(wVersionRequested, &wsaData); // 初始化Socket API（Winsock）
+    
     if (err != 0)
     {
         // 找不到 winsock.dll
         printf("加载 winsock 失败，错误代码为: %d\n", WSAGetLastError());
         return FALSE;
     }
+    
     if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2)
     {
         printf("不能找到正确的 winsock 版本\n");
         WSACleanup();
         return FALSE;
     }
-    ProxyServer = socket(AF_INET, SOCK_STREAM, 0);
+    
+    ProxyServer = socket(AF_INET, SOCK_STREAM, 0);  // 创建套接字，返回描述符。使用TCP协议（用于监听来自客户端的连接）
+    
     if (INVALID_SOCKET == ProxyServer)
     {
         printf("创建套接字失败，错误代码为： %d\n", WSAGetLastError());
         return FALSE;
     }
+    
     ProxyServerAddr.sin_family = AF_INET;
     ProxyServerAddr.sin_port = htons(ProxyPort);
     ProxyServerAddr.sin_addr.S_un.S_addr = INADDR_ANY;
-    if (bind(ProxyServer, (SOCKADDR *)&ProxyServerAddr, sizeof(SOCKADDR)) == SOCKET_ERROR)
+    
+    if (bind(ProxyServer, (SOCKADDR *)&ProxyServerAddr, sizeof(SOCKADDR)) == SOCKET_ERROR)  // 绑定套接字的本地端点地址，IP为所有可用IP，端口为ProxyPort
     {
         printf("绑定套接字失败\n");
         return FALSE;
     }
+    
     if (listen(ProxyServer, SOMAXCONN) == SOCKET_ERROR)
     {
         printf("监听端口%d 失败", ProxyPort);
@@ -119,6 +135,7 @@ BOOL InitSocket()
     }
     return TRUE;
 }
+
 //************************************
 // Method: ProxyThread
 // FullName: ProxyThread
@@ -136,17 +153,27 @@ unsigned int __stdcall ProxyThread(LPVOID lpParameter)
     int length = sizeof(SOCKADDR_IN);
     int recvSize;
     int ret;
+
+    // 接受客户端发送的数据
     recvSize = recv(((ProxyParam*)lpParameter)->clientSocket,Buffer, MAXSIZE, 0);
     HttpHeader *httpHeader = new HttpHeader(); // 移动至if之前，否则会报错
+    
+    // 接受失败或连接被关闭则报错
     if (recvSize <= 0)
     {
         goto error;
     }  
+
+    // 动态分配缓存空间，并复制接受到的数据
     CacheBuffer = new char[recvSize + 1];
     ZeroMemory(CacheBuffer, recvSize + 1);
     memcpy(CacheBuffer, Buffer, recvSize);
+    
+    // 解析HTTP头部信息
     ParseHttpHead(CacheBuffer, httpHeader);
     delete CacheBuffer;
+
+    // 将请求转发到目标服务器
     if (!ConnectToServer(&((ProxyParam*)lpParameter)->serverSocket,httpHeader->host))
     {
         goto error;
@@ -162,6 +189,7 @@ unsigned int __stdcall ProxyThread(LPVOID lpParameter)
     }
     // 将目标服务器返回的数据直接转发给客户端
     ret = send(((ProxyParam*)lpParameter)->clientSocket,Buffer, sizeof(Buffer), 0);
+
 // 错误处理
 error:
     printf("关闭套接字\n");
@@ -172,6 +200,7 @@ error:
     _endthreadex(0);
     return 0;
 }
+
 //************************************
 // Method: ParseHttpHead
 // FullName: ParseHttpHead
@@ -185,27 +214,32 @@ void ParseHttpHead(char *buffer, HttpHeader *httpHeader)
 {
     char *p;
     char *ptr;
-    const char *delim = "\r\n";
+    const char *delim = "\r\n"; // 分隔符：HTTP头部每行的结束符
+    
     p = strtok_s(buffer, delim, &ptr); // 提取第一行
     printf("%s\n", p);
+    
+    // 请求方法：GET or POST
     if (p[0] == 'G')
     { // GET 方式
         memcpy(httpHeader->method, "GET", 3);
-        memcpy(httpHeader->url, &p[4], strlen(p) - 13);
+        memcpy(httpHeader->url, &p[4], strlen(p) - 13); // 从请求行中提取URL
     }
     else if (p[0] == 'P')
     { // POST 方式
         memcpy(httpHeader->method, "POST", 4);
-        memcpy(httpHeader->url, &p[5], strlen(p) - 14);
+        memcpy(httpHeader->url, &p[5], strlen(p) - 14); // 从请求行中提取URL
     }
     printf("%s\n", httpHeader->url);
+    
+    // 循环提取后续部分，包括主机名HOST、Cookie
     p = strtok_s(NULL, delim, &ptr);
     while (p)
     {
         switch (p[0])
         {
         case 'H': // Host
-            memcpy(httpHeader->host, &p[6], strlen(p) - 6);
+            memcpy(httpHeader->host, &p[6], strlen(p) - 6); // 提取主机名
             break;
         case 'C': // Cookie
             if (strlen(p) > 8)
@@ -215,7 +249,7 @@ void ParseHttpHead(char *buffer, HttpHeader *httpHeader)
                 memcpy(header, p, 6);
                 if (!strcmp(header, "Cookie"))
                 {
-                    memcpy(httpHeader->cookie, &p[8], strlen(p) - 8);
+                    memcpy(httpHeader->cookie, &p[8], strlen(p) - 8);   // 提取cookie
                 }
             }
             break;
@@ -225,6 +259,7 @@ void ParseHttpHead(char *buffer, HttpHeader *httpHeader)
         p = strtok_s(NULL, delim, &ptr);
     }
 }
+
 //************************************
 // Method: ConnectToServer
 // FullName: ConnectToServer
